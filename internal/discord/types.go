7710d45968f74e62ac811bdcf38c75841c508a6d
Heribio <ethan.levratti@icloud.com>
2025-03-07 18:09:24 +0100

    Reset, make a local chat client

--- internal/discord/types.go
+++
@@ -1,7 +0,0 @@
-package discord
-
-type Secrets struct {
-    WEBHOOK_URL string `json:"WEBHOOK_URL"`
-    BOT_TOKEN string `json:"BOT_TOKEN"`
-    BOT_ID string `json:"BOT_ID"`
-}
--- pkg/discordWebhook/types.go
+++
@@ -1,6 +0,0 @@
-package discordWebhook
-
-type Message struct {
-    Content string `json:"content"`
-    Username string `json:"username"`
-}
